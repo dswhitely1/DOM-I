cce205c6bd9a9084d8518402377dc3f7a48bf130
Donald Whitely <dswhitely1@gmail.com>
2019-05-13 09:05:01 -0400

    MVP Finished

--- js/index.js
+++ js/index.js
@@ -1,68 +1,76 @@
 const siteContent = {
-  "nav": {
-    "nav-item-1": "Services",
-    "nav-item-2": "Product",
-    "nav-item-3": "Vision",
-    "nav-item-4": "Features",
-    "nav-item-5": "About",
-    "nav-item-6": "Contact",
-    "img-src": "img/logo.png"
-  },
-  "cta": {
-    "h1": "DOM Is Awesome",
-    "button": "Get Started",
-    "img-src": "img/header-img.png"
-  },
-  "main-content": {
-    "features-h4":"Features",
-    "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "about-h4":"About",
-    "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "middle-img-src": "img/mid-page-accent.jpg",
-    "services-h4":"Services",
-    "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "product-h4":"Product",
-    "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-    "vision-h4":"Vision",
-    "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
-  },
-  "contact": {
-    "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
-    "phone" : "1 (888) 888-8888",
-    "email" : "sales@greatidea.io",
-  },
-  "footer": {
-    "copyright" : "Copyright Great Idea! 2018"
-  },
+	nav            : {
+		'nav-item-1' : 'Services',
+		'nav-item-2' : 'Product',
+		'nav-item-3' : 'Vision',
+		'nav-item-4' : 'Features',
+		'nav-item-5' : 'About',
+		'nav-item-6' : 'Contact',
+		'img-src'    : 'img/logo.png',
+	},
+	cta            : {
+		h1        : 'DOM Is Awesome',
+		button    : 'Get Started',
+		'img-src' : 'img/header-img.png',
+	},
+	'main-content' : {
+		'features-h4'      : 'Features',
+		'features-content' :
+			'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+		'about-h4'         : 'About',
+		'about-content'    :
+			'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+		'middle-img-src'   : 'img/mid-page-accent.jpg',
+		'services-h4'      : 'Services',
+		'services-content' :
+			'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+		'product-h4'       : 'Product',
+		'product-content'  :
+			'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+		'vision-h4'        : 'Vision',
+		'vision-content'   :
+			'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.',
+	},
+	contact        : {
+		'contact-h4' : 'Contact',
+		address      : '123 Way 456 Street Somewhere, USA',
+		phone        : '1 (888) 888-8888',
+		email        : 'sales@greatidea.io',
+	},
+	footer         : {
+		copyright : 'Copyright Great Idea! 2018',
+	},
 };
 
 // Example: Update the img src for the logo
-let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+let logo = document.getElementById('logo-img');
+logo.setAttribute('src', siteContent['nav']['img-src']);
 
-
-const navLinks = document.querySelectorAll("a");
-navLinks.forEach((element,i) => (element.textContent = Object.values(siteContent.nav)[i]));
-const ctaH1 = document.querySelector("h1");
+const navLinks = document.querySelectorAll('a');
+navLinks.forEach((element, i) => (element.textContent = Object.values(siteContent.nav)[i]));
+const ctaH1 = document.querySelector('h1');
 ctaH1.textContent = siteContent.cta.h1;
-const ctaButton = document.querySelector("button");
+const ctaButton = document.querySelector('button');
 ctaButton.textContent = siteContent.cta.button;
-const ctaImage = document.getElementById("cta-img");
-ctaImage.setAttribute('src', siteContent["cta"]["img-src"])
-const mainContent = Object.values(siteContent["main-content"]);
-const textContentH4 = document.querySelectorAll("h4");
-let h4Index = [0,2,5,7,9]
+const ctaImage = document.getElementById('cta-img');
+ctaImage.setAttribute('src', siteContent['cta']['img-src']);
+const mainContent = Object.values(siteContent['main-content']);
+const textContentH4 = document.querySelectorAll('h4');
+let h4Index = [ 0, 2, 5, 7, 9 ];
 
-textContentH4.forEach((element, i) => (element.textContent=mainContent[h4Index[i]]))
-textContentP = document.querySelectorAll("p");
+textContentH4.forEach((element, i) => (element.textContent = mainContent[h4Index[i]]));
+textContentP = document.querySelectorAll('p');
 console.log(textContentP);
-h4Index = [1,3,6,8,10];
-h4Index.forEach((element, i)=> (textContentP[i].textContent = mainContent[element]))
-const middleImg = document.getElementById("middle-img");
-middleImg.setAttribute('src', mainContent[4])
+h4Index = [ 1, 3, 6, 8, 10 ];
+h4Index.forEach((element, i) => (textContentP[i].textContent = mainContent[element]));
+const middleImg = document.getElementById('middle-img');
+middleImg.setAttribute('src', mainContent[4]);
 
 const contactContent = Object.values(siteContent.contact);
-const contactSection = document.querySelector(".contact");
+const contactSection = document.querySelector('.contact').children;
 
-contactSection.forEach((element,i) => element.textContent = contactContent[i]);
+contactContent.forEach((element, i) => (contactSection[i].textContent = element));
+
+const copyRight = document.querySelector('footer').children;
+
+copyRight[0].textContent = siteContent['footer']['copyright'];
